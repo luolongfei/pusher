bd8397d2ced28230b5e34b6e688306e589166c29
luolongfei <mybsdc@gmail.com>
2020-01-19 16:16:51 +0800

    优化环境变量的获取逻辑

--- app/helpers.php
+++ app/helpers.php
@@ -155,41 +155,14 @@ if (!function_exists('env')) {
     /**
      * 获取环境变量值
      *
-     * @param $key
-     * @param null $default
+     * @param string $key
+     * @param string $default 默认值
      *
      * @return array|bool|false|null|string
      */
-    function env($key, $default = null)
+    function env($key = '', $default = null)
     {
-        Env::instance()->load();
-
-        $value = getenv($key);
-
-        if ($value === false) {
-            return $default;
-        }
-
-        switch (strtolower($value)) {
-            case 'true':
-            case '(true)':
-                return true;
-            case 'false':
-            case '(false)':
-                return false;
-            case 'empty':
-            case '(empty)':
-                return '';
-            case 'null':
-            case '(null)':
-                return null;
-        }
-
-        if (($valueLength = strlen($value)) > 1 && $value[0] === '"' && $value[$valueLength - 1] === '"') { // 去除双引号
-            return substr($value, 1, -1);
-        }
-
-        return $value;
+        return Env::instance()->get($key, $default);
     }
 }
 
--- libs/Env.php
+++ libs/Env.php
@@ -21,27 +21,52 @@ class Env
     /**
      * @var array 环境变量值
      */
-    protected static $val;
+    protected $allValues;
 
-    public function __construct()
+    public function __construct($fileName)
     {
+        $this->allValues = Dotenv::create(ROOT_PATH, $fileName)->load();
     }
 
-    public static function instance()
+    public static function instance($fileName = '.env')
     {
-        if (!self::$instance instanceof Env) {
-            self::$instance = new self();
+        if (!self::$instance instanceof self) {
+            self::$instance = new self($fileName);
         }
 
         return self::$instance;
     }
 
-    public function load($fileName = '.env')
+    public function get($key = '', $default = null)
     {
-        if (self::$val === null) {
-            self::$val = Dotenv::create(ROOT_PATH, $fileName)->load();
+        if (!strlen($key)) { // 不传key则返回所有环境变量
+            return $this->allValues;
         }
 
-        return self::$val;
+        $value = getenv($key);
+        if ($value === false) {
+            return $default;
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return null;
+        }
+
+        if (($valueLength = strlen($value)) > 1 && $value[0] === '"' && $value[$valueLength - 1] === '"') { // 去除双引号
+            return substr($value, 1, -1);
+        }
+
+        return $value;
     }
 }
